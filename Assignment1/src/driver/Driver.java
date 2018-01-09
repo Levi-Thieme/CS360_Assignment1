c44f918851732ae6bbc3281c8815ec91c33673fb
Levi-Thieme <thielt01@students.ipfw.edu>
2018-01-09 18:10:02 -0500

    Added TableController, DatabaseView, Driver, and Database Classes

---
+++ Assignment1/src/driver/Driver.java
@@ -0,0 +1,12 @@
+package driver;
+
+import controllers.TableController;
+import views.DatabaseView;;
+
+public class Driver {
+	
+	
+	public static void main(String[] args) {
+		TableController tbController = new TableController("No file");
+	}
+}
