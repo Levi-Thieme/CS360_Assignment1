3ee063ee56d9501e3adf359285e6404a7d0d8091
zacharytom <zacharytom@zacharys-mbp.ad.ipfw.edu>
2018-01-10 14:25:52 -0500

    updates

--- Assignment1/src/Model/Watershed.java
+++ Assignment1/src/Model/Watershed.java
@@ -8,7 +8,7 @@ public class Watershed {
 	private String name;
 	private String location_description;
 	private double latitude, longitude;
-	Date updated = new Date();
+	
 	
 	public Watershed(int id, String n, String location_description, double lat, double longitude) {
 		
@@ -17,6 +17,47 @@ public class Watershed {
 		this.location_description = location_description;
 		latitude = lat;
 		this.longitude = longitude;
-		updated.getTime();
+
 	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation_description() {
+		return location_description;
+	}
+
+	public void setLocation_description(String location_description) {
+		this.location_description = location_description;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
 }
