4f6b3c726196e39c0a8e880fd764346a6727f2cb
Levi-Thieme <thielt01@students.ipfw.edu>
2018-01-11 14:39:06 -0500

    modified controller and added outlines for methods

--- Assignment1/src/controllers/ApplicationController.java
+++ Assignment1/src/controllers/ApplicationController.java
@@ -1,5 +1,57 @@
 package controllers;
 
-public class ApplicationController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ApplicationController implements MouseListener, ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		/*
+		 * Need to handle all of the ApplicationFrame's menuItems and buttons in this
+		 */
+		
+	}
+
+	/**
+	 * Handles marker clicks
+	 */
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		/*
+		 * Check if e.getSource() is an instanceof Marker
+		 * If so, pass e.getSource to the displayInfo() method
+		 */
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
 
 }
